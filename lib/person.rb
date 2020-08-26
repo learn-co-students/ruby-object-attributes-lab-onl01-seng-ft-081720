a75d7aaf8cd01c6cb5b039fd0aa6a39689b1857a
SophiaGrace16 <sophia.g.white@outlook.com>
2020-08-26 17:56:06 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,40 @@
+class Person
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
+end
+
+
+
+
+
+
+
+
+
+# Person
+# is defined within lib/person.rb (FAILED - 6)
+# with names
+#   #name=
+#     writes the name of the person to an instance variable @name (FAILED - 7)
+#   #name
+#     reads the name of the person from an instance variable @name (FAILED - 8)
+# with jobs
+#   #job=
+#     writes the job of the person to an instance variable @job (FAILED - 9)
+#   #job
+#     reads the job of the person from an instance variable @job (FAILED - 10)
